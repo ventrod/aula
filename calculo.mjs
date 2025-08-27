0b66b3e2165500f26c0fbe396d014b50d8317a8a
ventrod <100435260+ventrod@users.noreply.github.com>
2025-08-27 19:40:08 -0300

    Add files via upload

---
+++ calculo.mjs
@@ -0,0 +1,5 @@
+import { somar, subtrair } from './funcao.mjs';
+
+console.log(somar(2,3));
+
+console.log(subtrair(2,3));
---
+++ funcao.mjs
@@ -0,0 +1,8 @@
+export function somar (n1, n2){ 
+   return n1 + n2
+}
+
+export function subtrair (n1, n2){
+    return n1 - n2 
+}
+
